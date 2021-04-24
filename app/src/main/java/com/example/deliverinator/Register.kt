2b355d78b604d8668c2c0c13d7f92f42ec9ffb40
Florin <florinmihu31@gmail.com>
2021-04-24 13:03:12 +0300

    Refactor Login XML

--- app/src/main/java/com/example/deliverinator/Register.kt
+++ app/src/main/java/com/example/deliverinator/Register.kt
@@ -2,13 +2,10 @@ package com.example.deliverinator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        Log.d("Test", "Test branch")
+        setContentView(R.layout.activity_register)
     }
 }
