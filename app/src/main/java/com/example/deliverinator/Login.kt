eaf793222549e898c19449ccdbf4ba7769581ba1
Florin Mihu <53407990+florinmihu31@users.noreply.github.com>
2021-04-25 11:00:06 +0300

    Add Register here action (#3)
    * Add Register here action
    
    * Refactor launchRegisterActivity in Login.kt

--- app/src/main/java/com/example/deliverinator/Login.kt
+++ app/src/main/java/com/example/deliverinator/Login.kt
@@ -1,11 +1,18 @@
 package com.example.deliverinator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun launchRegisterActivity(view: View) {
+        intent = Intent(this, Register::class.java)
+        startActivity(intent)
     }
 }
