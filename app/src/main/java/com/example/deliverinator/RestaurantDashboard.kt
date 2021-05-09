394f1cc768a15a8dab8d934c534c059103adcd03
Stefan <stefan.m.constandache@gmail.com>
2021-05-09 17:23:06 +0300

    Separated users dashboards

---
+++ app/src/main/java/com/example/deliverinator/RestaurantDashboard.kt
@@ -0,0 +1,11 @@
+package com.example.deliverinator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RestaurantDashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant_dashboard)
+    }
+}
