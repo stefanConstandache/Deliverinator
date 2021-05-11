27e6487e2dff546bd257646645de6ffdcd3f29f6
Florin <florinmihu31@gmail.com>
2021-05-11 17:22:12 +0300

    Update file structure

--- app/src/main/java/com/example/deliverinator/SplashScreen.kt
+++ app/src/main/java/com/example/deliverinator/SplashScreen.kt
@@ -3,9 +3,10 @@ package com.example.deliverinator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import android.widget.Toast
+import com.example.deliverinator.admin.AdminDashboard
+import com.example.deliverinator.client.ClientDashboard
+import com.example.deliverinator.restaurant.RestaurantDashboard
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
