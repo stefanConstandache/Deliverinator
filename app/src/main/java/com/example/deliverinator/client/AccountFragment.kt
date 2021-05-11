7f02373f1877353d0dbc26206a4b8b12780f1b8d
Stefan <stefan.m.constandache@gmail.com>
2021-05-11 18:59:09 +0300

    Renamed fragments files.

--- app/src/main/java/com/example/deliverinator/client/AccountFragment.kt
+++ app/src/main/java/com/example/deliverinator/client/AccountFragment.kt
@@ -22,7 +22,7 @@ class AccountFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        return inflater.inflate(R.layout.client_fragment_account, container, false)
     }
 
 }
