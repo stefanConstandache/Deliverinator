cc290f830554654c826b27b308ef6b5bf765650e
Florin <florinmihu31@gmail.com>
2021-05-14 00:08:13 +0300

    Refactor code, implement restaurant account

--- app/src/main/java/com/example/deliverinator/restaurant/AccountFragment.kt
+++ app/src/main/java/com/example/deliverinator/restaurant/AccountFragment.kt
@@ -6,19 +6,25 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import com.example.deliverinator.IMAGE_PICK_CODE
-import com.example.deliverinator.PERMISSION_CODE
-import com.example.deliverinator.R
+import com.example.deliverinator.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class AccountFragment : Fragment() {
     private lateinit var mAccountImageView: ImageView
+    private lateinit var mRestaurantName: TextView
+    private lateinit var mDescription: TextView
+    private lateinit var mChangePassword: Button
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mStore: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +34,24 @@ class AccountFragment : Fragment() {
         val view = inflater.inflate(R.layout.restaurant_fragment_account, container, false)
 
         mAccountImageView = view.findViewById(R.id.account_fragment_imageView)
+        mRestaurantName = view.findViewById(R.id.account_fragment_name)
+        mDescription = view.findViewById(R.id.account_fragment_description)
+        mChangePassword = view.findViewById(R.id.account_fragment_button)
+        mAuth  = FirebaseAuth.getInstance()
+        mStore = FirebaseFirestore.getInstance()
+
+        val user = mAuth.currentUser
+        val docRef = mStore.collection(USERS).document(user!!.uid)
+
+        docRef.get().addOnSuccessListener { mRestaurantName.text = it.getString(NAME) }
+
+        mDescription.setOnClickListener {
+            setDescription(it)
+        }
+
+        mChangePassword.setOnClickListener {
+            sendChangePasswordEmail()
+        }
 
         mAccountImageView.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -50,6 +74,40 @@ class AccountFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun sendChangePasswordEmail() {
+        mAuth.sendPasswordResetEmail(mAuth.currentUser!!.email!!)
+            .addOnSuccessListener {
+                Toast.makeText(context, R.string.reset_link_sent, Toast.LENGTH_SHORT)
+                    .show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(context, getString(R.string.link_not_sent) + it.message, Toast.LENGTH_LONG)
+                    .show()
+            }
+    }
+
+    private fun setDescription(it: View?) {
+        val descriptionField = EditText(it?.context)
+        val descriptionDialog = it?.context?.let { context -> AlertDialog.Builder(context) }
+
+        if (!mDescription.text.equals(getString(R.string.no_description))) {
+            descriptionField.setText(mDescription.text.toString())
+        }
+
+        descriptionDialog
+            ?.setTitle(R.string.add_description)
+            ?.setView(descriptionField)
+            ?.setPositiveButton(R.string.set) { _, _ ->
+                val description = descriptionField.text.toString().trim()
+
+                if (description.isEmpty()) {
+                    mDescription.text = getString(R.string.no_description)
+                } else {
+                    mDescription.text = description
+                }
+            }?.create()?.show()
     }
 
     private fun chooseImageFromGallery() {
