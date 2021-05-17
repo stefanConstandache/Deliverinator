968e9fa508e4059146045f4140cce0668762a80a
Florin <florinmihu31@gmail.com>
2021-05-17 23:40:01 +0300

    Database retrieve not working

--- app/src/main/java/com/example/deliverinator/restaurant/MenuFragment.kt
+++ app/src/main/java/com/example/deliverinator/restaurant/MenuFragment.kt
@@ -3,39 +3,45 @@ package com.example.deliverinator.restaurant
 import android.Manifest
 import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
-import android.content.ContentResolver
+import android.content.ClipDescription
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.MimeTypeMap
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.deliverinator.IMAGE_PICK_CODE
-import com.example.deliverinator.MENU_ITEMS
 import com.example.deliverinator.PERMISSION_CODE
 import com.example.deliverinator.R
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.restaurant_add_item_dialog.view.*
 import kotlinx.android.synthetic.main.restaurant_fragment_menu.view.*
 
+const val foodUriString = "android.resource://com.example.deliverinator/drawable/ic_food"
+
 class MenuFragment : Fragment(), MenuAdapter.OnItemClickListener {
     private lateinit var mAdapter: MenuAdapter
     private lateinit var mMenuItems: ArrayList<MenuItem>
     private lateinit var mDialogImageView: ImageView
+    private lateinit var mItemName: TextView
+    private lateinit var mItemDescription: TextView
+    private lateinit var mAuth: FirebaseAuth
     private lateinit var mStorageRef: StorageReference
     private lateinit var mDatabaseRef: DatabaseReference
-    private lateinit var mImageUri: Uri
+    private var mImageUri: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,14 +50,44 @@ class MenuFragment : Fragment(), MenuAdapter.OnItemClickListener {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.restaurant_fragment_menu, container, false)
 
-        mMenuItems = generateDummyList()
-        mAdapter = MenuAdapter(mMenuItems, this)
-        mStorageRef = FirebaseStorage.getInstance().getReference(MENU_ITEMS)
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference(MENU_ITEMS)
+        mAuth = FirebaseAuth.getInstance()
+        mStorageRef = FirebaseStorage.getInstance().getReference(mAuth.currentUser?.uid!!)
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference(mAuth.currentUser?.uid!!)
 
-        view.restaurant_fragment_recyclerView.adapter = mAdapter
-        view.restaurant_fragment_recyclerView.layoutManager = LinearLayoutManager(context)
-        view.restaurant_fragment_recyclerView.setHasFixedSize(true)
+        mMenuItems = ArrayList()
+
+        val x = this
+
+        mDatabaseRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (postSnapshot in snapshot.children) {
+                    val upload = postSnapshot.getValue(UploadMenuItem::class.java)
+
+                    if (upload != null) {
+                        mMenuItems.add(
+                            MenuItem(
+                                Uri.parse(upload.imageUrl),
+                                upload.itemName,
+                                upload.itemDescription,
+                                true
+                            )
+                        )
+                    }
+
+                    Toast.makeText(context, upload?.itemName!!, Toast.LENGTH_SHORT).show()
+                }
+
+                mAdapter = MenuAdapter(mMenuItems, x)
+
+                view.restaurant_fragment_recyclerView.adapter = mAdapter
+                view.restaurant_fragment_recyclerView.layoutManager = LinearLayoutManager(context)
+                view.restaurant_fragment_recyclerView.setHasFixedSize(true)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+            }
+        })
 
         view.restaurant_fragment_fab.setOnClickListener {
             addMenuItem(view)
@@ -66,6 +102,8 @@ class MenuFragment : Fragment(), MenuAdapter.OnItemClickListener {
         val dialogLayout = inflater.inflate(R.layout.restaurant_add_item_dialog, null)
 
         mDialogImageView = dialogLayout.restaurant_add_dialog_imageView
+        mItemName = dialogLayout.restaurant_add_name
+        mItemDescription = dialogLayout.restaurant_add_description
 
         dialogLayout.restaurant_add_button.setOnClickListener {
             chooseImage()
@@ -73,20 +111,34 @@ class MenuFragment : Fragment(), MenuAdapter.OnItemClickListener {
 
         builder.setTitle(R.string.add_item)
             .setPositiveButton(R.string.add) { _, _ ->
-                val newItem = MenuItem(
-                    mImageUri,
-                    dialogLayout.restaurant_add_name.text.toString(),
-                    dialogLayout.restaurant_add_description.text.toString(),
-                    true
-                )
+                val newItem: MenuItem?
 
-                uploadFile()
+                if (mImageUri != null) {
+                    newItem = MenuItem(
+                        mImageUri!!,
+                        dialogLayout.restaurant_add_name.text.toString(),
+                        dialogLayout.restaurant_add_description.text.toString(),
+                        true
+                    )
+
+                    uploadFile()
+                } else {
+                    mImageUri = Uri.parse(foodUriString)
+
+                    newItem = MenuItem(
+                        mImageUri!!,
+                        dialogLayout.restaurant_add_name.text.toString(),
+                        dialogLayout.restaurant_add_description.text.toString(),
+                        true
+                    )
+                }
+
+                mImageUri = null
 
                 mMenuItems.add(newItem)
                 mAdapter.notifyItemInserted(mMenuItems.size - 1)
             }
-            .setNegativeButton(R.string.cancel) { _, _ ->
-            }
+            .setNegativeButton(R.string.cancel, null)
 
         val dialog = builder.create()
 
@@ -96,11 +148,15 @@ class MenuFragment : Fragment(), MenuAdapter.OnItemClickListener {
 
     private fun uploadFile() {
         val fileReference = mStorageRef.child(System.currentTimeMillis().toString() + "." +
-                getFileExtension(mImageUri))
+                getFileExtension(mImageUri!!))
 
-        fileReference.putFile(mImageUri)
+        fileReference.putFile(mImageUri!!)
             .addOnSuccessListener {
-                val upload = UploadMenuItem(it.storage.downloadUrl.toString())
+                val upload = UploadMenuItem(
+                    it.storage.downloadUrl.toString(),
+                    mItemName.text.toString().trim(),
+                    mItemDescription.text.toString().trim()
+                )
                 val uploadId = mDatabaseRef.push().key
 
                 if (uploadId != null) {
@@ -120,20 +176,6 @@ class MenuFragment : Fragment(), MenuAdapter.OnItemClickListener {
         val mime = MimeTypeMap.getSingleton()
 
         return mime.getExtensionFromMimeType(contentResolver?.getType(uri))
-    }
-
-    private fun generateDummyList(): ArrayList<MenuItem> {
-        val list = ArrayList<MenuItem>()
-
-        /*val burger = MenuItem(R.drawable.burger, "Burger", "Cel mai dulce si frumos burger", true)
-        val shaorma = MenuItem(R.drawable.shaorma, "Shaorma", "Shaorma cu de toate pentru fetele tunate", true)
-        val pizza = MenuItem(R.drawable.pizza, "Pizza", "Pizza ca a lu' Tanti Mitza", true)
-
-        list += burger
-        list += shaorma
-        list += pizza*/
-
-        return list
     }
 
     override fun onItemClick(position: Int) {
--- app/src/main/java/com/example/deliverinator/restaurant/UploadMenuItem.kt
+++ app/src/main/java/com/example/deliverinator/restaurant/UploadMenuItem.kt
@@ -1,3 +1,22 @@
 package com.example.deliverinator.restaurant
 
-class UploadMenuItem(val imageUrl: String)
+class UploadMenuItem() {
+    private var mImageUrl: String = ""
+    private var mItemName: String = ""
+    private var mItemDescription: String = ""
+
+    val imageUrl
+        get() = mImageUrl
+
+    val itemName
+        get() = mItemName
+
+    val itemDescription
+        get() = mItemDescription
+
+    constructor(imageUrl: String, itemName: String, itemDescription: String) : this() {
+        this.mImageUrl = imageUrl
+        this.mItemName = itemName
+        this.mItemDescription = itemDescription
+    }
+}
