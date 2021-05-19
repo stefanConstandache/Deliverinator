c7ca56e3c04be7d8e1c55130db26a8451ddbfc7b
Florin <florinmihu31@gmail.com>
2021-05-19 23:44:35 +0300

    Remove unused imports

--- app/src/main/java/com/example/deliverinator/restaurant/MenuAdapter.kt
+++ app/src/main/java/com/example/deliverinator/restaurant/MenuAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.*
 import com.example.deliverinator.R
 import com.squareup.picasso.Picasso
