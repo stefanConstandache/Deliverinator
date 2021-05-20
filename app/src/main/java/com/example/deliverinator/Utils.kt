894285b518b74f2d5a5ceef67c3b44ec0a42439e
stefanConstandache <79802742+stefanConstandache@users.noreply.github.com>
2021-05-20 17:58:33 +0300

    Implemented delete account for client feature. (#51)
    * Implemented register
    
    * Separated users dashboards
    
    * Changed register options
    
    * Added icons for client menu.
    Added tag menu for client dashboard.
    
    * Added menu folder.
    
    * Added client fragments.
    
    * Renamed fragments files.
    
    * Little changes
    
    * Added account tab on client dashboard.
    The user can now update his account information.
    
    * Added errors if fields are empty or if the phone is invalid.
    
    * Rearranged the order of information in account tab.
    
    * Implemented RecyclerView for restaurant tab with static list of items.
    
    * Implemented new design for restaurant account and apply changes button.
    
    * Implemented delete account for client feature.
    
    Co-authored-by: Bogdan Popescu <30412621+bogdan9926@users.noreply.github.com>

--- app/src/main/java/com/example/deliverinator/Utils.kt
+++ app/src/main/java/com/example/deliverinator/Utils.kt
@@ -2,10 +2,12 @@ package com.example.deliverinator
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.text.TextUtils
 import android.util.Patterns
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
 
 val passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+\$).{8,}\$".toRegex()
--- app/src/main/java/com/example/deliverinator/admin/AddFragment.kt
+++ app/src/main/java/com/example/deliverinator/admin/AddFragment.kt
@@ -120,9 +120,12 @@ class AddFragment : Fragment() {
 
                 restaurantInfo[NAME] = name
                 restaurantInfo[RESTAURANT_DESCRIPTION] = ""
+                restaurantInfo[EMAIL] = email
+
                 restaurantRef.set(restaurantInfo)
 
                 mProgressBar.visibility = View.INVISIBLE
+
                 mName.text.clear()
                 mEmail.text.clear()
                 mPassword.text.clear()
