baea207f6128ee207a8a64e09e84edce8af0aa4f
Stefan <stefan.m.constandache@gmail.com>
2021-05-23 23:43:32 +0300

    Update UI for restaurant item.

--- app/src/main/java/com/example/deliverinator/restaurant/MenuAdapter.kt
+++ app/src/main/java/com/example/deliverinator/restaurant/MenuAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CompoundButton
 import androidx.recyclerview.widget.RecyclerView.*
 import com.example.deliverinator.R
 import com.example.deliverinator.UploadMenuItem
